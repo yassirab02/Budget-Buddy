759f6d8eefa06f3cd3ea94502cef74190f1fd806
yassirab02 <yassirabourial86@gmail.com>
2024-11-24 20:55:27 +0100

    adding the rest of the files

---
+++ budget-buddy-api/src/main/java/com/yassir/expensestracker/user/User.java
@@ -0,0 +1,81 @@
+package com.yassir.expensestracker.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "\"user\"")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails , Principal {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private boolean accountLocked;
+    private boolean enabled;
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
+}
